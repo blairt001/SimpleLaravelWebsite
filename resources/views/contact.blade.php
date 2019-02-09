a754ef28805b6779266667fc752f2024b83f0079
Tony Okoth <blairt371.dev@gmail.com>
2019-02-09 16:24:57 +0300

    Added contact fields

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,12 +2,23 @@
 
 @section('content')
 <h1>Contact</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+{!! Form::open(['url' => 'contact/submit']) !!}
+   <div class="form-group">
+   	{{Form::label('name', 'E-Mail Address: ')}}
+   	{{Form::text('name','Enter name',['class'=>'form-control', 'placeholder'=>'Enter name'])}}
+   </div>
+   <div class="form-group">
+   	{{Form::label('email', 'E-Mail Address: ')}}
+   	{{Form::text('email', 'Enter E-Mail',['class'=>'form-control', 'placeholder'=>'Enter Email'])}}
+   </div>
+   <div class="form-group">
+   	{{Form::label('message', 'Message: ')}}
+   	{{Form::textarea('message', 'Enter Message',['class'=>'form-control','placeholder'=>'Enter message'])}}
+   </div>
+   <div>
+   	{{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+   </div>
+{!! Form::close() !!}
 @endsection
 
 @section('sidebar')
